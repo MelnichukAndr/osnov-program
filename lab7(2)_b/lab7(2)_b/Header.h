7e117605bc30da17ee0f8279a18b494d449cbf76
Андрій Мельничук <Andrii.Melnychuk@cs.khpi.edu.ua>
2022-05-06 14:53:35 +0300

    7b

---
+++ lab7(2)_b/lab7(2)_b/Header.h
@@ -0,0 +1,5 @@
+#pragma once
+void FeelMas(int* mas, int size);
+void PrintMas(int* mas, int size);
+void WriteF1(int* mas, int s, const char* fname);
+int SumElement(int* mas2, int s, const char* fname);
